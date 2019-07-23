b318d610dd47e964660fb5d26561cf6f8bdc8717
Niall Mackinnon <Niall.Mackinnon@microsoft.com>
2019-07-23 19:01:13 +0000

    Merged PR 1532: Read input data from serial
    The format of the data is 5 bytes, the first 4 bytes correspond to digital input where 0 is off and anything greater than 0 is on. The last byte is the analog input that can go from 0 to 255.

--- data-merge/springboard/springboard.ino
+++ data-merge/springboard/springboard.ino
@@ -1,31 +1,76 @@
-const int maxButtons = 4;
-const int basePin = 3;
-int button = 0;
+const int maxDigitalButtons = 4;
+const int baseDigitalPin = 3;
+const int analogPin = 9;
+
+bool hasNewData = false;
+bool pinValue[maxDigitalButtons];
 
 // Runs once when you press reset or power the board
-void setup() {
+void setup()
+{
+  Serial.begin(9600);
+
+  memset(pinValue, 0, sizeof(pinValue));
+
   // Initialize digital pin LED_BUILTIN as an output
   pinMode(LED_BUILTIN, OUTPUT);  
 
   // Initialize digital pins for our buttons as output
-  for (int i = 0; i < maxButtons; ++i)
+  for (int i = 0; i < maxDigitalButtons; ++i)
   {
-    pinMode(basePin + i, OUTPUT);
+    pinMode(baseDigitalPin + i, OUTPUT);
   }
+
+  pinMode(9, OUTPUT);
 }
 
 // Runs over and over again forever
-void loop() {
-  // Turn the button on for 1 second
-  digitalWrite(LED_BUILTIN, HIGH);
-  digitalWrite(basePin + button % maxButtons, HIGH);
-  delay(1000);
-  
-  // Turn the button off for 1 second
-  digitalWrite(LED_BUILTIN, LOW);
-  digitalWrite(basePin + button % maxButtons, LOW);
-  delay(1000);
+void loop()
+{
+  readInput();
+  setOutput();
+}
 
-  // Advance which button we'll drive on the next iteration
-  button++;
+void readInput()
+{
+  int availableBytes = Serial.available();
+  if (availableBytes > 0)
+  {
+    uint8_t buffer[5] = {0};
+    Serial.readBytes(buffer, sizeof(buffer));
+
+    for (int i = 0; i < maxDigitalButtons; ++i)
+    {
+      pinValue[i] = buffer[i] > 0;
+    }
+
+    pinValue[maxDigitalButtons] = buffer[maxDigitalButtons];
+
+    hasNewData = true;
+  }
+}
+
+void setOutput()
+{
+  digitalWrite(LED_BUILTIN, LOW);
+
+  if (hasNewData)
+  {
+    // Update the digital pins, which are packed in the front
+    // of the values array.
+    for (int i = 0; i < maxDigitalButtons; ++i)
+    {
+      digitalWrite(baseDigitalPin + i, pinValue[i] > 0 ? HIGH : LOW);
+    }
+
+    // Update the analog pin, which is the last value in the
+    // array.
+    analogWrite(analogPin, pinValue[maxDigitalButtons]);
+    
+    // Debug LED for when we process data.
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // We've processed the new data, so clear the flag.
+    hasNewData = false;
+  }
 }
